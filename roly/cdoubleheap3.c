e34499f0ad8a8e62bdfac5799d1144ca6a6cb3de
Keith Goodman <kwgoodman@gmail.com>
2011-06-02 09:07:18 -0700

    Group public functions together in DH3.

--- roly/cdoubleheap3.c
+++ roly/cdoubleheap3.c
@@ -51,26 +51,6 @@ struct mm_handle {
 };
 
 
-/* 
- * Construct the double heap with the given total number of values. 
- * 
- * Arguments:
- * len -- The total number of values in the double heap. 
- *
- * Return: The mm_handle structure, uninitialized. 
- */
-struct mm_handle *mm_new(const npy_uint64 len) {
-  struct mm_handle *mm = malloc(sizeof(struct mm_handle));
-  mm->n_l = 0;
-  mm->n_s = 0;
-  mm->nodes = malloc(len * sizeof(struct mm_node*));
-  mm->node_data  = malloc(len * sizeof(struct mm_node));
-  mm->s_heap = mm->nodes;
-  mm->l_heap = &mm->nodes[len/2 + len % 2];
-  return mm;
-}
-
-
 /*
  * Return the smallest child node of the given node, if it doesn't exist, 
  * return the node itself. 
@@ -209,6 +189,7 @@ inline void move_up_small(struct mm_node **heap,
   }
 }
 
+
 /*
  * Move the given node down through the heap to the appropriate position. 
  */ 
@@ -264,6 +245,33 @@ inline void swap_heap_heads(struct mm_handle *mm) {
   mm->l_heap[0] = s_node;
   move_up_large(mm->l_heap, mm->n_l, s_node);
   move_down_small(mm->s_heap, mm->n_s, l_node);
+}
+
+
+/*
+ *   ----------------
+ *   Public functions
+ *   ----------------
+ */
+
+
+/* 
+ * Construct the double heap with the given total number of values. 
+ * 
+ * Arguments:
+ * len -- The total number of values in the double heap. 
+ *
+ * Return: The mm_handle structure, uninitialized. 
+ */
+struct mm_handle *mm_new(const npy_uint64 len) {
+  struct mm_handle *mm = malloc(sizeof(struct mm_handle));
+  mm->n_l = 0;
+  mm->n_s = 0;
+  mm->nodes = malloc(len * sizeof(struct mm_node*));
+  mm->node_data  = malloc(len * sizeof(struct mm_node));
+  mm->s_heap = mm->nodes;
+  mm->l_heap = &mm->nodes[len/2 + len % 2];
+  return mm;
 }
 
 
@@ -368,6 +376,7 @@ inline npy_float64 mm_get_median_odd(struct mm_handle *mm) {
 inline npy_float64 mm_get_median_even(struct mm_handle *mm) {
   return (mm->s_heap[0]->val + mm->l_heap[0]->val) / 2;
 }
+
 
 /*
  * Print the two heaps to the screen.
